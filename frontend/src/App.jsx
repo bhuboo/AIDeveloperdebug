0d7cdf5d42651d45f61eb9928fa382be938cf1e5
MaheshboopathiM <mahesh.boopathi@usistech.com>
2025-01-23 18:42:57 +0530

    message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,22 @@ import "./index.css";
 import AppRoutes from "./routes/AppRoutes";
 import UserProvider from "./context/UserContext";
 import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {  
   return (
-    <StrictMode>
-  <UserProvider>
-    <AppRoutes />
-  </UserProvider>
-    </StrictMode>
-
+  //   <StrictMode>
+  // <UserProvider>
+  //   <AppRoutes />
+  // </UserProvider>
+  //   </StrictMode>
+<StrictMode>
+  <BrowserRouter>
+  <Suspense>
+    <AppRoutes/>
+  </Suspense>
+  </BrowserRouter>
+</StrictMode>
   );
 }
 
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,23 +3,41 @@ import Login from '../screen/Login'
 import Register from '../screen/Register'
 import Home from '../screen/Home'
 import Project from '../screen/Project'
-import {BrowserRouter,Route, Routes} from 'react-router-dom'
+import {BrowserRouter,Route, Routes,useRoutes} from 'react-router-dom'
 import UserAuth from '../auth/UserAuth'
 
 function AppRoutes() {
-  return (
-        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <Routes>
-            {/* <Route path="/" element={ <Home/> } /> */}
-            <Route path="/" element={ <UserAuth><Home/></UserAuth> } />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/register" element={<Register/>} />
-            {/* <Route path="/project" element={<Project/>} /> */}
-            <Route path="/project" element={<UserAuth><Project/></UserAuth>} />
-            <Route path="*" element={<Login/>} />
-        </Routes>
-        </BrowserRouter>
-  )
+  return useRoutes(
+    [
+      {
+        path: "/",
+        element: <UserAuth><Home/></UserAuth>
+      },
+      {
+        path: '/login',
+        element:<Login/>
+      },
+      {
+        path: '/register',
+        element:<Register/>
+      },
+      {
+        path: '/project',
+        element:<UserAuth><Project/></UserAuth>
+      }
+    ]
+  );
+        // <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        // <Routes>
+        //     {/* <Route path="/" element={ <Home/> } /> */}
+        //     <Route path="/" element={ <UserAuth><Home/></UserAuth> } />
+        //     <Route path="/login" element={<Login/>} />
+        //     <Route path="/register" element={<Register/>} />
+        //     {/* <Route path="/project" element={<Project/>} /> */}
+        //     <Route path="/project" element={<UserAuth><Project/></UserAuth>} />
+        //     <Route path="*" element={<Login/>} />
+        // </Routes>
+        // </BrowserRouter>
 }
 
 export default AppRoutes
